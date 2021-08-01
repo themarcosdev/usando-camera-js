516f0e3b548b2ab08753ec627decc9e0646c3098
themarcosdev <>
2021-08-01 06:42:48 +0000

    usando html2canvas para tirar fotos do projeto

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 https://www.html5rocks.com/en/tutorials/getusermedia/intro/ <br>
-O link acima traz referereferências de como usar o WebRTC, a api que é responsáresponsável  pela nossa captura de vídeo no dispositivo. 
+O link acima traz referereferências de como usar o WebRTC, a api que é responsáresponsável  por mostrar o vídeo do nosso dispositivo. 
--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-
+// usando a api webRTC para abrir a câmera 
 
 function iniciarVideoDaCam(){
-  navigator.mediaDevices.getUserMedia({video:true}).then(stream=>{
+
+  const specs = {video:{width:"fotoDiv"}}  
+
+  navigator.mediaDevices.getUserMedia(specs).then(stream=>{
     const videoElement = document.querySelector("#video")
     videoElement.srcObject = stream
   }).catch(error=>{console.log(error)})
@@ -9,3 +12,19 @@ function iniciarVideoDaCam(){
 }
 
 window.addEventListener("DOMContentLoaded",iniciarVideoDaCam)
+
+// usando html2canvas 
+ // Definindo qual div vms fotografar
+        function tirarFoto() {
+            let div =
+                document.getElementById('video');
+  
+            // Usando html2canvas que tira fotos e envia para a div fotoPronta
+            html2canvas(div).then(
+                function (canvas) {
+                    document
+                    .getElementById('fotoPronta')
+                    .appendChild(canvas);
+                })
+        }
+
